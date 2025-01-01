d33ea5740eea467eb463d61e677394ce7148ee2b
JS2IIU <makoto.harazawa@gmail.com>
2025-01-01 11:33:21 +0900

    version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adiftools',
-    version='0.0.1',
+    version='0.0.2',
     description='ADIF file utilities',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
