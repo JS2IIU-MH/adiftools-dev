c71c5d9e1f26a16669e285fcfab24d3dac29b945
JS2IIU <makoto.harazawa@gmail.com>
2025-01-02 18:19:49 +0900

    fix flake8 error

--- tests/test_adiftools.py
+++ tests/test_adiftools.py
@@ -7,6 +7,7 @@ at = adiftools.ADIFParser()
 file_path = 'tests/sample.adi'
 df = at.read_adi(file_path)
 
+
 def test_read_adi():
     assert df.shape == (126, 14)
     assert df.columns.tolist() == [
@@ -25,11 +26,12 @@ def test_plot_band_percentage():
     at.plot_band_percentage('tests/percentage_band_test.png')
     assert True
 
+
 def test_number_of_records():
     at = adiftools.ADIFParser()
     file_path = 'tests/sample.adi'
     _ = at.read_adi(file_path)
     assert at.number_of_records == 126
 
-# TODO: add test_plot_monthly()
+
 # TODO: use test fixture to create a temporary file
