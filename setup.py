8ee62fa15d32da8482403331bcbdf491f0b720c7
JS2IIU <makoto.harazawa@gmail.com>
2025-01-05 14:42:06 +0900

    v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='adiftools',
-    version='0.0.5',
+    version='0.1.0',
     description='ADIF file utilities',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
@@ -12,7 +12,9 @@ setup(
     maintainer_email='info@js2iiu.com',
     url='https://github.com/JS2IIU-MH/adiftools-dev',
     download_url='https://github.com/JS2IIU-MH/adiftools-dev',
-    packages=find_packages(include=['adiftools', 'adiftools.*'], exclude=['test/']),
+    packages=find_packages(
+        include=['adiftools', 'adiftools.*'],
+        exclude=['test/']),
     # package_dir = {"": "adiftools"},
     install_requires=open('requirements.txt').readlines(),
     keywords=['adif'],
