607832e0443d71b1a39476a3d8f9895eeecec582
JS2IIU-MH <makoto.harazawa@gmail.com>
2025-08-10 22:19:09 +0900

    update: add test code for monthly_band_qso

--- tests/test_adifgraph.py
+++ tests/test_adifgraph.py
@@ -13,3 +13,8 @@ def test_monthly_qso():
 def test_band_percentage():
     adifgraph.band_percentage(df, 'tests/percentage_band.png')
     assert True
+
+
+def test_monthly_band_qso():
+    adifgraph.monthly_band_qso(df, 'tests/monthly_band_qso.png')
+    assert True
